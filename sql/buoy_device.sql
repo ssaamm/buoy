e0a10205aae9e86a116803e87a2299459012ef96
Samuel Taylor <github@samueltaylor.org>
2015-05-01 15:35:12 -0500

    Add (lat, lon) FK to buoy on buoy device

--- sql/buoy_device.sql
+++ sql/buoy_device.sql
@@ -6,6 +6,8 @@ CREATE TABLE `buoy_device` (
   `device_id` int(11) NOT NULL,
   PRIMARY KEY (`latitude`,`longitude`,`device_id`),
   KEY `fk_buoy_device_1` (`device_id`),
-  CONSTRAINT `fk_buoy_device_1` FOREIGN KEY (`device_id`) REFERENCES `device` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `fk_buoy_device_2` (`latitude`,`longitude`),
+  CONSTRAINT `fk_buoy_device_1` FOREIGN KEY (`device_id`) REFERENCES `device` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_buoy_device_2` FOREIGN KEY (`latitude`, `longitude`) REFERENCES `buoy` (`latitude`, `longitude`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
 
